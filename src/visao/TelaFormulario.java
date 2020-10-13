ed5b6a55436376ce9c6f41907636356baca36705
Gabriel Delfino <47621308+nunesdelfino@users.noreply.github.com>
2020-10-13 19:48:36 -0300

    Atualziacao

--- src/visao/TelaFormulario.java
+++ src/visao/TelaFormulario.java
@@ -1,6 +1,5 @@
 package visao;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 
 import javax.swing.JFrame;
--- src/visao/TelaLista.java
+++ src/visao/TelaLista.java
@@ -8,7 +8,6 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import visao.controle.VisaoControlePrincipal;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
